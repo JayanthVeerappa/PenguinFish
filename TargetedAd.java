3607417a37841da6424f4384f0cb3d772c4bb04c
Nikhil K <nikhil9kalburgi@gmail.com>
2026-02-19 19:48:02 -0800

    add comments for code clarity

--- TargetedAd.java
+++ TargetedAd.java
@@ -1,74 +1,52 @@
 /*
  * Problem 2.3.1 Sell My Pet Food
  */
-public class TargetedAd {
+public class TargetedAd
+{
 
   public static void main(String[] args)
   {
-    /*  
-     * TODO:
-     * PREPARATION WORK
-     * (1) Create a file called targetWords.txt. Populate this file with words on each line that
-     *     you think would determine if a user is a dog or cat owner.
-     * 
-     * PROGRAMMING
-     * (2) Create a new DataCollector object and set the data to "socialMediaPostsSmall.txt" and "targetWords.txt"
-     *     Important: Use the socialMedialPostsSmall to create your algorithm. Using a small file will help you 
-     *     generate your solution quicker and give you the ability to double check your work.
-     * (3) Create a String variable to hold the names of all the user. (The first word of every post is 
-     *     a person's username)
-     * (4) Compare each user's post to each target word. If a user mentions a target word, add their username to 
-     *     the String of users. Separate usernames with a space. 
-     *         Hint: You can use loops to look through each word. 
-     *         Hint2: You can use indexOf to check if a word is in a user post. 
-     * (5) Once you have all the users, use your DataCollector's prepareAdvertisement method to prepare a file 
-     *     with all users and the advertisement you will send them.
-     *         Additional Info: The prepareAdvertisement creates a new file on your computer. Check the posts of
-     *         some of the usernames to make sure your algorithm worked.
-     * 
-     * THE FINAL SOLUTION
-     * (6) Your solution should work with the socialMedialPostsSmall.txt. Modify your DataCollector initialization
-     *    so you use the socialMediaPosts.txt. You should now have a larger file of users to target.
-     */
 
-    
+    DataCollector dc = new DataCollector();
+    // Calls setData() to load social media posts
+    dc.setData("socialMediaPostsSmall.txt", "targetWords.txt");
 
-    /* your code here */
-    
-    DataCollector dataCollector = new DataCollector();
-    
-
-    dataCollector.setData("socialMediaPosts.txt","targetWords.txt");
-    System.out.println(dataCollector.getNextPost());
-    
+    // build list of usernames
     String matchedUsers = "";
-    String post;
     
-    while (!((post) = dataCollector.getNextPost()).equals("NONE"))
+    // iterate through posts
+    String post;
+
+    while (!(post = dc.getNextPost()).equals("NONE"))
       {
-        String LowerPost = post.toLowerCase();
+        // Extracts username
         String username = post.split(" ")[0];
 
+        // Calls getNextTargetWord() accessor to iterateate through target words
         String word;
-        while (!(word = dataCollector.getNextTargetWord()).equals("NONE"))
+        while (!(word = dc.getNextTargetWord()).equals("NONE"))
           {
-            if (LowerPost.indexOf(word) == -1)
+            // Uses indexOf() to check if target word appears
+            if (post.indexOf(word) != -1)
               {
+                // Checks for duplicates
                 if (matchedUsers.indexOf(username) == -1)
                   {
+                    // username with space separator
                     matchedUsers += username + " ";
                   }
-                  break;
+                break;
               }
           }
       }
 
-    String advertisement = "Check out our premium pet food - specially formulated for your feathery friend!";
-    dataCollector.prepareAdvertisement("targetedUsers.txt", matchedUsers, advertisement);
+    //message
+    String advertisement = "Check out our premium penguin food - specially formulated for your feathery friend!";
+    
+    // write output file
+    dc.prepareAdvertisement("targetedUsers.txt", matchedUsers, advertisement);
     
     System.out.println("Advertisement file created: targetedUsers.txt");
     System.out.println("Targeted users: " + matchedUsers);
   }
 }
-  
-
